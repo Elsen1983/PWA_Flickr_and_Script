902ea80110a067b834ad119324b06a3e4753c28b
Norbert Korom <norbertkorom@gmail.com>
2020-05-12 19:00:05 +0100

    add few functions
    some new functions added for later works

--- js/app.js
+++ js/app.js
@@ -24,7 +24,15 @@ function init() {
             //add event listeners to the buttons
             newB.addEventListener("click", function () {
                 console.log("button pressed -> add " + searchTerms[i] + " into searchTerms");
-                //getImages(searchTerms[i]);
+                //if app is not online --> offline, so load the last loaded images to the screen
+                if (!navigator.onLine) {
+                    console.log("App is offline, so add last loaded images to the page");
+
+                }
+                //app is online
+                else{
+                    getImages(searchTerms[i]);
+                }
             });
         }
         loadFromStorage();
@@ -35,18 +43,266 @@ function init() {
     }
 }
 
-/* ----------------------- Check if any search terms are in local storage. ----------------------- */
+/* ----------------------- load and check the localStorage ----------------------- */
 function loadFromStorage() {
     console.log('%c 1 - loadFromStorage called', 'background: #222; color: #bada55');
-    let last_search_terms = localStorage.getItem("last_search");
-    if (last_search_terms != null) {
-        console.log("LOCALSTORAGE is NOT empty.");
-        //getImages(last_search_terms);
-    } else {
-        console.log("LOCALSTORAGE is EMPTY.");
+
+    //app is offline
+    if (!navigator.onLine) {
+
+        console.log("App is offline, so add last loaded images to the page if loaded earlier");
+
+        let last_search_terms = window.localStorage.getItem("last_search");
+        let images_LocalStorage = window.localStorage.getItem("images");
+
+        /*  check that the app used earlier or not*/
+        if (last_search_terms != null) {
+            console.log("LOCALSTORAGE is NOT empty. Used earlier.");
+            //load the last loaded images to the screen if possible
+            images_LocalStorage = (images_LocalStorage) ? JSON.parse(images_LocalStorage) : [];
+            showImages(images_LocalStorage);
+        }
+        else{
+            console.log("LOCALSTORAGE is EMPTY.");
+        }
+
+
+        //get images from localstorage
+        //var images_LocalStorage = localStorage.getItem("images");
+        if(!images_LocalStorage){
+            console.log("images not stored in localStorage earlier");
+        }
+        else{
+            console.log("images stored in localStorage earlier");
+            images_LocalStorage = (images_LocalStorage) ? JSON.parse(images_LocalStorage) : [];
+            console.log(images_LocalStorage);
+
+        }
+
+    }
+    //app is online
+    else{
+        let last_search_terms = window.localStorage.getItem("last_search");
+        if (last_search_terms != null) {
+            console.log("LOCALSTORAGE is NOT empty.");
+            getImages(last_search_terms);
+        } else {
+            console.log("LOCALSTORAGE is EMPTY.");
+        }
     }
 }
 
+/* ----------------------- Search Flickr - getImages -----------------------*/
+function getImages(searchTermText) {
+
+    console.log('%c  2 - getImage() called with ' + searchTermText, 'background: #222; color: #bada55');
+
+    window.localStorage.setItem('last_search', searchTermText);
+
+    let base_url = "https://www.flickr.com/services/rest?";
+    let request = "method=flickr.photos.search";
+    request += "&per_page=10";
+    request += "&api_key=22303ae620835b04e5c55988c44ed6d3";
+    request += "&text=" + escape(searchTermText);
+    request += "&format=json&jsoncallback=showImages";
+    request += "&tag_mode=all";
+    let full = base_url + request;
+
+    /*  To avoid adding script tags every time into the DOM when the user presses a button
+        we check all the script tag and remove that "flickr script" which is the same like
+        that generated above (full) */
+    let all_script = document.getElementsByTagName("script");
+
+    for(let i=0; i < all_script.length; i++) {
+        if (all_script[i].src.toString() !== full.toString()) {
+            //do nothing because the script is not in the DOM
+        }
+        else{
+            //remove the same script tag
+            all_script[i].parentNode.removeChild( all_script[i] );
+        }
+    }
+    //add back as last one again
+    let scriptTag = document.createElement("script");
+    scriptTag.setAttribute("src", full);
+    scriptTag.setAttribute("type", "application/javascript");
+    let bodyZero = document.querySelectorAll("body")[0];
+    // fetch(scriptTag.src).then(function (response) {
+    //     console.log(response);
+    //
+    // })
+    bodyZero.appendChild(scriptTag);
+    disableButtons();
+}
+
+/* -------------------Handle FlickrSearch Results ------------------*/
+function showImages(images) {
+    console.log(images);
+    console.log("%c  3 - showImages called", 'background: #222; color: #bada55');
+    if(!navigator.onLine){
+        console.log("OFFLINE");
+    }
+
+    let flickrImageArray = [];
+    document.getElementById('content_div').innerHTML = "";
+    if (images.stat === 'fail') {
+        document.getElementById('content_div').innerHTML = "Error";
+        enableButtons();
+    } else {
+        /* clear the images array, because we want to store just the last searchTerm + images in the localStorage*/
+        images_Array.length = 0;
+
+        let targetnr = images.photos.photo.length;
+        if (targetnr === 0) {
+            document.getElementById('content_div').innerHTML = "No Results";
+            enableButtons();
+        } else {
+            for (let i = 0; i < images.photos.photo.length; i++) {
+                let url = "https://farm" + images.photos.photo[i].farm;
+                let imgName = "/";
+                let title = images.photos.photo[i].title;
+
+                //if(caches.match())
+
+                url += ".static.flickr.com";
+
+                imgName += images.photos.photo[i].server + "/";
+                imgName += images.photos.photo[i].id + "_";
+                imgName += images.photos.photo[i].secret;
+                let big_Url = url + imgName + ".jpg";
+                // console.log("Image: " + imgName + ".jpg");
+                let image_name_extension = imgName + ".jpg";
+
+                /*  Add the current images into the 'images_Array' with name+extension */
+                images_Array.push(image_name_extension);
+
+                //clear the images in localStorage
+                removeItemFromLocalStorage("images");
+                //add current images into localStorage
+                addItemToLocalStorage("images", JSON.stringify(images_Array));
+
+                //??need here to check the net connection is ready or not
+                flickrImageArray.push(new FlickrImage(big_Url, title, image_name_extension));
+            }
+            createImages(flickrImageArray);
+
+        }
+    }
+}
+
+/* - Constructor for FlickrImages object*/
+function FlickrImage(image_URL, image_Title, image_NameAndExtension) {
+    console.log("FlickrImage constructor called");
+    this.imageSRC = image_URL;
+    this.imageTitle = image_Title;
+    this.imageName = image_NameAndExtension;
+}
+
+FlickrImage.prototype.getImageObject = function () {
+    console.log("FlickrImage.prototype.getImageObject called");
+    //console.log("return promises: " + this.imageSRC);
+
+    return new Promise((resolve, reject) => {
+        // Create an image object
+        let imageObject = new Image();
+        imageObject.src = this.imageSRC;
+
+        document.getElementById("loading_txt").style.display = "none";
+
+        imageObject.onload = () => {
+
+            imageObject.onclick = () => {
+                console.log("Modal activated on clicked image :" + this.imageTitle);
+                imageTitleOnModal(this.imageTitle)
+            };
+
+            resolve(imageObject);
+        };
+
+        imageObject.onerror = () => {
+            imageObject.innerHTML = "Can't Load";
+            reject();
+        };
+    });
+};
+
+
+
+function imageTitleOnModal(getTitle) {
+    // console.log("getModalAndZoomImages called with title: " + getTitle);
+    const modalPlace = document.getElementById("body_Tag");
+    let modal = document.createElement("div");
+    modal.setAttribute("id", "modal");
+    let subModal = document.createElement("div");
+    subModal.setAttribute("id", "subModal");
+    let titleDiv = document.createElement("div");
+    titleDiv.id = "modal_title_box";
+    let titleText = document.createElement("p");
+    titleText.textContent = getTitle;
+    titleDiv.appendChild(titleText);
+    modal.onclick = function () {
+        modalPlace.removeChild(modalPlace.lastChild);
+    };
+    subModal.appendChild(titleDiv);
+    modal.appendChild(subModal);
+    modalPlace.appendChild(modal);
+}
+
+
+
+
+
+
+/* ------------------------ Place Images on the Page ------------ */
+function createImages(flickr_array) {
+    console.log("createImages called");
+    let promiseArray = [];
+    for (let i = 0; i < flickr_array.length; i++) {
+        let divObj = document.createElement("li");
+        divObj.className = "imageTag";
+        let loader = document.createElement("img");
+        loader.src = 'img/loaderGif.gif';
+        loader.className = "loader";
+        divObj.appendChild(loader);
+        let tempPromise = flickr_array[i].getImageObject();
+        tempPromise.then(function (resolvedImage) {
+            divObj.innerHTML = "";
+            divObj.appendChild(resolvedImage);
+        });
+        promiseArray.push(tempPromise);
+        document.getElementById('content_div').appendChild(divObj);
+    }
+    Promise.all(promiseArray).then(enableButtons);
+
+
+    // Note: This suffices for this lab. In reality there is an issue with
+    // an image not being found. I.e. the corresponding Promise will not
+    // resolve, therefore all() cannot be invoked and the buttons
+    // will remain disabled
+
+    // However, We will leave this solution here in order to see how all() works
+    // (and because Flickr is reliable enough for our purposes )
+
+    // Possible solutions include using a timeout to enable the buttons after
+    // a certain time has elapsed
+}
+
+/* ----------------- Enable and Display search buttons ----------- */
+function enableButtons() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.cursor = "pointer"; //This makes it clickable
+        // buttons[i].style.pointerEvents = "pointer";
+        buttons[i].style.opacity = "1"; //This grays it out to look not disabled
+    }
+}
+
+function disableButtons() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.cursor = "none"; //This makes it not clickable
+        // buttons[i].style.pointerEvents = "none";
+        buttons[i].style.opacity = "0.6"; //This grays it out to look disabled
+    }
+}
 
 
 
@@ -84,4 +340,12 @@ function resizePage() {
     if (window.innerWidth > 479) {
         x.style.display = "flex";
     }
+}
+
+function removeItemFromLocalStorage(key){
+    window.localStorage.removeItem(key);
+}
+
+function addItemToLocalStorage(key,value){
+    window.localStorage.setItem(key, value);
 }
--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-//my current cache name (version)
-var cache_Name = 'v1';
+/*  Current version of the cache */
+const cache_Name = 'v1';
 
-//immutable_cache_Files contains URLs that we know never change. These can safely be copied from cache to cache.
-//files that never change
-//looked for them in existing caches, and copied them directly to the new cache
-var immutable_cache_Files = [
+/*  The 'immutable_cache_Files' contains URLs that we know never change. These can safely be copied from cache to cache.
+    files that never change. Copy them directly to the new cache. */
+const immutable_cache_Files = [
     "./js/main.js",
     "./js/plugins.js",
     "https://fonts.googleapis.com/css?family=Baloo+Bhai+2:800&display=swap"
 ];
-//mutable_cache_Files contains URLs that we want to retrieve from the network every time we create a new cache.
-var mutable_cache_Files = [
+/*  The 'mutable_cache_Files' contains URLs that we want to retrieve from the network every time we create a new cache. */
+const mutable_cache_Files = [
     "./index.html",
     "./search2.html",
     "./manifest.webmanifest.json",
@@ -37,27 +36,28 @@ var mutable_cache_Files = [
  */
 self.addEventListener('install', function (e) {
     log(' Installing');
-    //the install event must wait until the promise within this is actually going to be resolved
+    /*  The install event must wait until the promise within this is actually going to be resolved */
     e.waitUntil(
-        //open 'cache_Name' from the caches
+        /* Open 'cache_Name' from the caches */
         caches
             .open(cache_Name)
             .then(function (cache) {
                 log(' caching immutable_cache_Files:');
-                //add all the default files into the cache
+                /* Add all the default files into the cache */
                 let new_immutable_cache_Files = [];
-                // install event first goes over all new_immutable_cache_Files and looks for them in all existing caches.
+                /*  Install event first goes over all new_immutable_cache_Files
+                    and looks for them in all existing caches. */
                 return Promise.all(
                     immutable_cache_Files.map(function (url) {
                         return caches.match(url).then(function (response) {
-                            //any requests that are found are copied to the new cache
+                            /*  Any requests that are found copied to the new cache */
                             if (response) {
                                 return cache.put(url, response);
                             }
-                            //those that are not  placed into the new_immutable_cache_Files array.
+                            /*  Those that are not placed into the new_immutable_cache_Files array */
                             else {
                                 new_immutable_cache_Files.push(url);
-                                //return promises
+                                /*  Return promises */
                                 return Promise.resolve();
                             }
                         });
@@ -83,13 +83,13 @@ self.addEventListener('activate', function (e) {
     /*  Wait until the following is done and only declare the service worker activated
         if all of the following complete successfully */
     e.waitUntil(
-        //get all the cache keys
+        /*  Get all the cache keys */
         caches.keys()
             .then(function (cacheNames) {
                 return Promise.all(cacheNames.map(function (key) {
-                    // If a cached item is saved under a previous cacheName
+                    /*  If a cached item is saved under a previous cacheName */
                     if (key !== cache_Name) {
-                        // Delete that cached file
+                        /*  Delete that cached file */
                         return caches.delete(key)
                             .then(function () {
                                 log(' removes cached Files (' + key + ') from cache.');
@@ -100,19 +100,26 @@ self.addEventListener('activate', function (e) {
                     return self.skipWaiting();
                 })
             })
-
     );
-    // Tell the active service worker to take control of the page immediately.
+    /*  Tell the active service worker to take control of the page immediately */
     return self.clients.claim();
 });
 
 
 /* ----------------------- 3 - Call 'fetch' event for the service worker ----------------------- */
+self.addEventListener('fetch', function (e) {
 
+    console.log("REQUEST TYPE - " + e.request.method);
+    /* if not a GET request */
+    if (e.request.method !== 'GET') {
+        log(" POST or PUT request!")
+    }
+
+});
 
 /* ----------------------- 4 - Functions ----------------------- */
 
-// each logging line will be prepended with the service worker version
+/*  Each logging line will be prepended with the service worker version */
 function log(message) {
     console.log("%c [ServiceWorker - '" + cache_Name + "]", 'background: #FFFFFF; color: #329011', message);
 }
